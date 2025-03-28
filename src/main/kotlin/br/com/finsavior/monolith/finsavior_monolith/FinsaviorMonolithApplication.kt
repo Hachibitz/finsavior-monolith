8894428ab221fa9fbf8ed38cba02ca9d9516f4bb
Hachibitz <felipealmeida.ns@outlook.com>
2025-03-28 20:05:02 -0300

    initial commit

---
+++ src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/FinsaviorMonolithApplication.kt
@@ -0,0 +1,11 @@
+package br.com.finsavior.monolith.finsavior_monolith
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinsaviorMonolithApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinsaviorMonolithApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/finsavior/monolith/finsavior_monolith/FinsaviorMonolithApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.finsavior.monolith.finsavior_monolith
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinsaviorMonolithApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
