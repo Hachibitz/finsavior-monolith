c390816a575dda1ee7660f89bf5abc64e5d1b7cf
Hachibitz <felipealmeida.ns@outlook.com>
2025-04-01 16:00:19 -0300

    retornando mais informações de perfil

--- src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/model/dto/ProfileDataDTO.kt
+++ src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/model/dto/ProfileDataDTO.kt
@@ -4,7 +4,10 @@ data class ProfileDataDTO (
     val username: String,
     val profilePicture: ByteArray? = null,
     val email: String,
-    val plan: PlanDTO
+    val plan: PlanDTO,
+    val firstName: String,
+    val lastName: String,
+    val name: String,
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -16,6 +19,9 @@ data class ProfileDataDTO (
         if (!profilePicture.contentEquals(other.profilePicture)) return false
         if (email != other.email) return false
         if (plan != other.plan) return false
+        if (firstName != other.firstName) return false
+        if (lastName != other.lastName) return false
+        if (name != other.name) return false
 
         return true
     }
@@ -25,6 +31,9 @@ data class ProfileDataDTO (
         result = 31 * result + (profilePicture?.contentHashCode() ?: 0)
         result = 31 * result + email.hashCode()
         result = 31 * result + plan.hashCode()
+        result = 31 * result + firstName.hashCode()
+        result = 31 * result + lastName.hashCode()
+        result = 31 * result + name.hashCode()
         return result
     }
 }
--- src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/model/mapper/UserMapper.kt
+++ src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/model/mapper/UserMapper.kt
@@ -5,14 +5,6 @@ import br.com.finsavior.monolith.finsavior_monolith.model.dto.SignUpDTO
 import br.com.finsavior.monolith.finsavior_monolith.model.entity.Audit
 import br.com.finsavior.monolith.finsavior_monolith.model.entity.User
 
-fun User.toUserDTO(): ProfileDataDTO =
-    ProfileDataDTO(
-        username = this.username,
-        profilePicture = this.userProfile!!.profilePicture,
-        email = this.userProfile!!.email,
-        plan = this.userPlan!!.plan.toPlanDTO(),
-    )
-
 fun SignUpDTO.toUser(): User =
     User(
         email = this.email,
@@ -31,5 +23,8 @@ fun User.toUserProfileDTO(): ProfileDataDTO =
         username = this.username,
         profilePicture = this.userProfile!!.profilePicture,
         email = this.userProfile!!.email,
-        plan = this.userPlan!!.plan.toPlanDTO()
+        plan = this.userPlan!!.plan.toPlanDTO(),
+        firstName = this.firstName,
+        lastName = this.lastName,
+        name = this.getFirstAndLastName(),
     )
