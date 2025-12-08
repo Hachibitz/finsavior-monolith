8ba156496e819dcdbc528d616f0f25b5875de476
Hachibitz <felipealmeida.ns@outlook.com>
2025-12-08 19:06:13 -0300

    fixing audio not working in android environment

--- src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/service/AiTranscriptionService.kt
+++ src/main/kotlin/br/com/finsavior/monolith/finsavior_monolith/service/AiTranscriptionService.kt
@@ -76,7 +76,7 @@ class AiTranscriptionService(
 
         val body = LinkedMultiValueMap<String, Any>()
         body.add("file", object : ByteArrayResource(audioFile.bytes) {
-            override fun getFilename(): String = "audio.m4a"
+            override fun getFilename(): String = "audio.mp3"
         })
         body.add("model", "whisper-1")
         body.add("language", "pt")
